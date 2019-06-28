5935c2c51bfe569fd2a29427e852edddefd3ee83
Alen Padurariu <alen.padurariu13@gmail.com>
2019-06-28 08:46:31 +0300

    Update

---
+++ java1.js
@@ -0,0 +1,79 @@
+// var clientAge = Number(prompt('Insert Age'));
+
+// if (clientAge <15) {
+//     alert('25% Reducere');
+// } else if(clientAge <25 && clientAge >=15) {
+//     alert('10% Reducere');
+// } else {
+//     alert ('Fara Reducere');
+// }
+
+// switch (clientAge) {
+//   case 15:
+//     alert("25% Reducere");
+//     break;
+//   case 16:
+//     alert("20% reducere");
+//     break;
+//   case 17:
+//     alert("15% reducere");
+//     break;
+//   case 18:
+//     alert("10% reducere");
+//     break;
+//   default:
+//     alert("Nici o reducere");
+// }
+
+// for(var i=0; i<100; i++){
+//     console.log(i+1)
+// }
+// for(var i=100; i>-2; i-=2){
+//     console.log(i)
+
+// let i=0;
+// while (i<10) {
+//     console.log(i);
+//     i=i+1;
+// }
+
+// var mySum;
+// for(var i=0; i<5; i++){
+//     mySum = i*(i+1);
+//     console.log(mySum);
+// }
+
+// var i=0;
+// do {
+//     console.log(i);
+//     i+=3
+// } while (i<10);
+
+// var myArray =[1,"mos craciun",{name:'Alen'},undefined,null,true]
+
+// console.log(typeof(myArray[0]));
+// console.log(typeof(myArray[1]));
+// console.log(typeof(myArray[2]));
+// console.log(typeof(myArray[3]));
+// console.log(typeof(myArray[4]));
+// console.log(typeof(myArray[5]));
+
+// // for (var i=0; i<myArray.length; i++){
+// //     console.log(typeof(myArray[i]));
+// }
+
+// var nr = [12, 3, 15, 1, 9, 90];       //minim of array//
+// var small=nr[0];
+
+// for(i=1; i<nr.length; i++){
+//     if(nr[i]<small) {
+//          small = nr[i];
+//     } 
+// }
+// console.log(small);
+
+// function calc(a,b){                 //funtions//
+//     var mySum=a-b;
+//     console.log('dif=',mySum);
+// }
+// calc(5,4)
